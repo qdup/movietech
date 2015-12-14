a726cc226e019ad80fc196893be2040b6b08e0c0
Gordon Wilkinson <gordonwilkinson@hotmail.com>
2015-12-14 12:06:53 -0800

    corrected news search column migration

--- db/migrate/20151213235830_add_news_search_to_sm_directory.rb
+++ db/migrate/20151213235830_add_news_search_to_sm_directory.rb
@@ -1,5 +1,5 @@
 class AddNewsSearchToSmDirectory < ActiveRecord::Migration
   def change
-    add_column :sm_directory, :news_search_term, :string
+    add_column :sm_directories, :news_search_term, :string
   end
 end
