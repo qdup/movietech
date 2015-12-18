86152dbc2d46a9be98aa500e13f1ecdc62ea4dbe
Gordon Wilkinson <gordonwilkinson@hotmail.com>
2015-12-18 08:29:44 -0800

    updated api action documentation

--- app/controllers/api/v1/sm_data_controller.rb
+++ app/controllers/api/v1/sm_data_controller.rb
@@ -1,23 +1,115 @@
 class Api::V1::SmDataController < Api::ApiController
   respond_to :json
 
-
-  # == GET A Social Media by TMDB_ID
+  # == GET  Social Media data collected for movie referenced by TMDB_ID
   # movietech.herokuapp.com/api/v1/sm_data/<tmdb_id>
   # eg: show all aggregated points for a specific tmdb_id: 276907
   # movietech.herokuapp.com/api/v1/sm_data/276907
-  # Headers:
+  # === Params
+  # tmdb_id: <tmdb_id>
+  # start_date: yyyy/mm/dd    (%Y/%m/%d) :optional
+  # end_date: yyyy/mm/dd    (%Y/%m/%d) :optional
+  #  === Headers
   # X-Api-Key   5sdZCBgJyfWBZwhnijxgQwtt
   # where 5sdZCBgJyfWBZwhnijxgQwtt is a valid admin user api_key (user.api_key: "5sdZCBgJyfWBZwhnijxgQwtt")  # === Response
   #   Returns json formatted movie object
   #   for the requested movie key where found.
-  # === Headers
-  #   Cache-Control:max-age=0, private, must-revalidate
-  #   Connection:Keep-Alive
-  #   Date:Sun, 07 Sep 2014 14:14:45 GMT
-  #   Etag:"2ed7df461fdda7d745e2f7d0a17e8f5e"
-  #   JSON body
-  def show
+  # === Response
+  # JSON
+  # Return social media data collected for the requested date(s). Where no date range is specifed a 7 day range is returned.
+  # {
+  #   "data_points": [
+  #     {
+  #       "id": 1413,
+  #       "created_at": "2015-11-30T00:01:26.917Z",
+  #       "updated_at": "2015-11-30T00:04:46.508Z",
+  #       "fb_id": "505861482904561",
+  #       "fb_likes": 494306,
+  #       "fb_talk_about": 182875,
+  #       "tmdb_id": 312221,
+  #       "twitter_id": "3253953780",
+  #       "twitter_statuses": 570,
+  #       "twitter_followers": 15128,
+  #       "movie_title": null,
+  #       "fb_page_name": "creedmovie",
+  #       "twitter_handle": "creedmovie",
+  #       "twitter_hashtag": "Creed",
+  #       "klout_id": "156781589595216925",
+  #       "release_date": null,
+  #       "date_key": "2015-11-30T00:00:00.000Z",
+  #       "klout_score": "79.5056354922966",
+  #       "klout_day_change": "0.246260171439275",
+  #       "klout_week_change": "7.74198417898903",
+  #       "klout_month_change": "13.0029090579688",
+  #       "inst_id": "253760693",
+  #       "inst_followed_by": 1207720,
+  #       "inst_follows": 24,
+  #       "inst_hash_tag": "Creed",
+  #       "inst_tag_media_count": 259960,
+  #       "inst_handle": "creedmovie",
+  #       "aggregate_score": "0.0237840288638437"
+  #     },
+  #     {
+  #       "id": 1475,
+  #       "created_at": "2015-12-01T00:01:09.990Z",
+  #       "updated_at": "2015-12-01T00:04:08.500Z",
+  #       "fb_id": "505861482904561",
+  #       "fb_likes": 498023,
+  #       "fb_talk_about": 165496,
+  #       "tmdb_id": 312221,
+  #       "twitter_id": "3253953780",
+  #       "twitter_statuses": 579,
+  #       "twitter_followers": 15435,
+  #       "movie_title": null,
+  #       "fb_page_name": "creedmovie",
+  #       "twitter_handle": "creedmovie",
+  #       "twitter_hashtag": "Creed",
+  #       "klout_id": "156781589595216925",
+  #       "release_date": null,
+  #       "date_key": "2015-12-01T00:00:00.000Z",
+  #       "klout_score": "79.5181665594634",
+  #       "klout_day_change": "0.0125310671668046",
+  #       "klout_week_change": "7.58396943870686",
+  #       "klout_month_change": "12.8689335793433",
+  #       "inst_id": "253760693",
+  #       "inst_followed_by": 1213042,
+  #       "inst_follows": 24,
+  #       "inst_hash_tag": "Creed",
+  #       "inst_tag_media_count": 263580,
+  #       "inst_handle": "creedmovie",
+  #       "aggregate_score": "0.0221260700829587"
+  #     }
+  #   ],
+  #   "daily_max_scores": [
+  #     {
+  #       "id": 17,
+  #       "max_ag_score": "0.138402844210364",
+  #       "datekey": "2015-11-30T00:00:00.000Z",
+  #       "created_at": "2015-11-30T00:04:48.916Z",
+  #       "updated_at": "2015-11-30T00:04:48.936Z",
+  #       "max_fb_likes": 22698645,
+  #       "max_fb_talk_about": 737949,
+  #       "max_twitter_followers": 1742705,
+  #       "max_inst_followed_by": 1207724
+  #     },
+  #     {
+  #       "id": 18,
+  #       "max_ag_score": "0.140732657380468",
+  #       "datekey": "2015-12-01T00:00:00.000Z",
+  #       "created_at": "2015-12-01T00:04:09.168Z",
+  #       "updated_at": "2015-12-01T00:04:09.177Z",
+  #       "max_fb_likes": 22698251,
+  #       "max_fb_talk_about": 742411,
+  #       "max_twitter_followers": 1744797,
+  #       "max_inst_followed_by": 1213044
+  #     }
+  #   ]
+  # }
+
+  # MovieMeter Sample call:
+  # MovieMeter::SocialData.find() class method handles requests to return social media data for the requesting tmdb_id for the optional start and end date
+  # sample class method find request: chartdata = SocialData.find('259694', 5.days.ago.to_date, 2.days.ago.to_date)
+  def show  #:nodoc:
     req_parm = {}
     no_response = {}
     req_parm[:tmdb_id] = params[:id]
--- app/controllers/api/v1/sm_directory_controller.rb
+++ app/controllers/api/v1/sm_directory_controller.rb
@@ -3,16 +3,20 @@ class Api::V1::SmDirectoryController < Api::ApiController
   respond_to :json
 
 
-  # == POST Track a new movie by TMDB_ID
+  # == POST SOCIAL MEDIA DIRECTORY for a new movie by TMDB_ID
   # === POST /api/v1/sm_directory/
-  # movietech.herokuapp.com/api/v1/sm_data/<tmdb_id>
-  #     adds movie referred by tmdb id where the the sm_directory table
+  # movietech.herokuapp.com/api/v1/sm_directory/<tmdb_id>
+  #     adds social media directory for movie referenced by tmdb id where the the sm_directory table
   #     is not found.
   # === Parameters
   #     tmdb_id: Integer(required). Example: 293660
   #     posts the movie titled Deadpool(2016)
   #       Request URL:http://localhost:9000/api/v1/sm_directory/
-  #   JSON body
+  # === Headers
+      #   Cache-Control:max-age=0, private, must-revalidate
+      #   Connection:Keep-Alive
+      #   Authorization: Token token=5sdZCBgJyfWBZwhnijxgQwtt
+  #   JSON body post
   # Sample body json payload:
   # {
   #   "tmdb_id": 293660,
@@ -26,6 +30,7 @@ class Api::V1::SmDirectoryController < Api::ApiController
   #   "release_date": "2016-09-09T00:00:00.000Z",
   #   "twitter_handle": "twitter handle 999999"
   # }
+
   def create #:nodoc:
     req_parm = {}
     no_response = {}
@@ -38,6 +43,23 @@ class Api::V1::SmDirectoryController < Api::ApiController
       respond_with :api, :v1, no_response, status: 404
     end
   end
+
+
+  # == PUT SOCIAL MEDIA DIRECTORY for an a TMDB id
+  # === PUT /api/v1/sm_directory/:tmdb_id
+  #     updates social media directory for movie referenced by tmdb id
+  #     Movie tmdb_id not modified.
+  # === Parameters
+  #     tmdb_id: Integer(required). Example: 293660
+  #     update the meta for the movie titled Deadpool(2016)
+  #       Request URL: http://movietech.herokuapp.com/api/v1/sm_directory/293660
+  # === Headers
+      #   Cache-Control:max-age=0, private, must-revalidate
+      #   Connection:Keep-Alive
+      #   Authorization: Token token=5sdZCBgJyfWBZwhnijxgQwtt
+  #   JSON body
+  # === Response
+  # JSON object: tmdb movie social media record
 
   def update #:nodoc:
     req_parm = {}
@@ -56,6 +78,20 @@ class Api::V1::SmDirectoryController < Api::ApiController
 
   end
 
+  # == GET SOCIAL MEDIA DIRECTORY for movie BY TMDB id
+  # === GET /api/v1/sm_directory/:tmdb_id
+  #     gets social media directory referenced by tmdb id
+  # === Parameters
+  #     tmdb_id: Integer(required). Example: 259694
+  #     update the meta for the movie titled How To Be Single
+  #       Request URL: http://movietech.herokuapp.com/api/v1/sm_directory/259694
+  # === Headers
+      #   Cache-Control:max-age=0, private, must-revalidate
+      #   Connection:Keep-Alive
+      #   Authorization: Token token=5sdZCBgJyfWBZwhnijxgQwtt
+  #   JSON body
+  # === Response
+  # JSON object: tmdb movie social media record
   def show
     req_parm = {}
     no_response = {}
